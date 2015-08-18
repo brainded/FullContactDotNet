7946fe82d87367f654cc5d62c6054081ceae742a
Adam Valverde <adamv@brushfireinteractive.com>
2015-08-18 14:55:05 -0700

    adding a unit test for lookup person by email

--- UnitTests/FullContactApiUnitTests.cs
+++ UnitTests/FullContactApiUnitTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FullContactDotNet;
+using System.Net;
 
 namespace UnitTests
 {
@@ -21,6 +22,17 @@ namespace UnitTests
         public void FullContactApiInitWithBlankApiKey()
         {
             var fullContactApi = new FullContactApi("   ");
+        }
+
+        [TestMethod]
+        public void FullContactInitAndLookupEmail()
+        {
+            var fullContactApi = new FullContactApi();
+            var actual = fullContactApi.LookupPersonByEmail("adam.dorado@gmail.com");
+
+            Assert.AreEqual(actual.Status, HttpStatusCode.OK, "FullContactApi returned an unexpected status looking up a person by email.");
+            Assert.AreEqual(actual.ContactInfo.GivenName, "Adam", "FullContactApi returned an unexpected given name looking up a person by email.");
+            Assert.AreEqual(actual.ContactInfo.FamilyName, "Dorado", "FullContactApi returned an unexpected family name looking up a person by email.");
         }
     }
 }
