a4671b5164ebd5028b1df10661fde4c0034b4276
Adam Valverde <adamv@brushfireinteractive.com>
2015-08-18 14:29:05 -0700

    adding queue and webhook optional params

--- FullContactDotNet/FullContactApi.cs
+++ FullContactDotNet/FullContactApi.cs
@@ -42,13 +42,15 @@ namespace FullContactDotNet
         /// Lookup Person by email address.
         /// </summary>
         /// <param name="emailAddress">The email address.</param>
+        /// <param name="queue">The queue.</param>
+        /// <param name="webhook">The webhook.</param>
         /// <returns></returns>
         /// <exception cref="System.ArgumentNullException">A email address is required to lookup a person by email.</exception>
-        public Person LookupPersonByEmail(string emailAddress)
+        public Person LookupPersonByEmail(string emailAddress, int? queue = null, string webhook = null)
         {
             if (string.IsNullOrEmpty(emailAddress)) throw new ArgumentNullException("A email address is required to lookup a person by email.");
 
-            var request = new RestRequest("/person.json", Method.GET);
+            var request = GetPersonRequest(queue, webhook);
             request.AddParameter("email", emailAddress);
             return Execute<Person>(request);
         }
@@ -58,15 +60,17 @@ namespace FullContactDotNet
         /// </summary>
         /// <param name="phoneNumber">The phone number.</param>
         /// <param name="countryCode">The country code.</param>
+        /// <param name="queue">The queue.</param>
+        /// <param name="webhook">The webhook.</param>
         /// <returns></returns>
         /// <exception cref="System.ArgumentNullException">A phone number is required to lookup a person by twitter.</exception>
-        public Person LookupPersonByPhone(string phoneNumber, string countryCode = null)
+        public Person LookupPersonByPhone(string phoneNumber, string countryCode = null, int? queue = null, string webhook = null)
         {
             if (string.IsNullOrEmpty(phoneNumber)) throw new ArgumentNullException("A phone number is required to lookup a person by twitter.");
 
             //TODO: investigate if the phone number needs to be formatted in a particular way
 
-            var request = new RestRequest("/person.json", Method.GET);
+            var request = GetPersonRequest(queue, webhook);
             request.AddParameter("phone", phoneNumber);
 
             if (!string.IsNullOrWhiteSpace(countryCode))
@@ -81,13 +85,15 @@ namespace FullContactDotNet
         /// Lookup Person by twitter username.
         /// </summary>
         /// <param name="twitterUsername">The twitter username.</param>
+        /// <param name="queue">The queue.</param>
+        /// <param name="webhook">The webhook.</param>
         /// <returns></returns>
         /// <exception cref="System.ArgumentNullException">A twitter username is required to lookup a person by twitter.</exception>
-        public Person LookupPersonByTwitter(string twitterUsername)
+        public Person LookupPersonByTwitter(string twitterUsername, int? queue = null, string webhook = null)
         {
             if (string.IsNullOrEmpty(twitterUsername)) throw new ArgumentNullException("A twitter username is required to lookup a person by twitter.");
 
-            var request = new RestRequest("/person.json", Method.GET);
+            var request = GetPersonRequest(queue, webhook);
             request.AddParameter("twitter", twitterUsername);
             return Execute<Person>(request);
         }
@@ -96,15 +102,40 @@ namespace FullContactDotNet
         /// Lookup Person by facebook username.
         /// </summary>
         /// <param name="facebookUsername">The facebook username.</param>
+        /// <param name="queue">The queue.</param>
+        /// <param name="webhook">The webhook.</param>
         /// <returns></returns>
         /// <exception cref="System.ArgumentNullException">A facebook username is required to lookup a person by facebook.</exception>
-        public Person LookupPersonByFacebook(string facebookUsername)
+        public Person LookupPersonByFacebook(string facebookUsername, int? queue = null, string webhook = null)
         {
             if (string.IsNullOrEmpty(facebookUsername)) throw new ArgumentNullException("A facebook username is required to lookup a person by facebook.");
 
-            var request = new RestRequest("/person.json", Method.GET);
+            var request = GetPersonRequest(queue, webhook);
             request.AddParameter("facebookUsername", facebookUsername);
             return Execute<Person>(request);
+        }
+
+        /// <summary>
+        /// Gets the person request.
+        /// </summary>
+        /// <param name="queue">The queue.</param>
+        /// <param name="webhook">The webhook.</param>
+        /// <returns></returns>
+        private RestRequest GetPersonRequest(int? queue = null, string webhook = null)
+        {
+            var request = new RestRequest("/person.json", Method.GET);
+
+            if (queue.HasValue)
+            {
+                request.AddParameter("queue", queue.Value);
+            }
+
+            if (!string.IsNullOrWhiteSpace(webhook))
+            {
+                request.AddParameter("webhook", webhook);
+            }
+
+            return request;
         }
 
         /// <summary>
--- FullContactDotNet/IFullContactApi.cs
+++ FullContactDotNet/IFullContactApi.cs
@@ -8,29 +8,37 @@ namespace FullContactDotNet
         /// Lookup Person by email address.
         /// </summary>
         /// <param name="emailAddress">The email address.</param>
+        /// <param name="queue">The queue.</param>
+        /// <param name="webhook">The webhook.</param>
         /// <returns></returns>
-        Person LookupPersonByEmail(string emailAddress);
+        Person LookupPersonByEmail(string emailAddress, int? queue = null, string webhook = null);
 
         /// <summary>
         /// Lookup Person by phone number.
         /// </summary>
         /// <param name="phoneNumber">The phone number.</param>
         /// <param name="countryCode">The country code.</param>
+        /// <param name="queue">The queue.</param>
+        /// <param name="webhook">The webhook.</param>
         /// <returns></returns>
-        Person LookupPersonByPhone(string phoneNumber, string countryCode = null);
+        Person LookupPersonByPhone(string phoneNumber, string countryCode = null, int? queue = null, string webhook = null);
 
         /// <summary>
         /// Lookup Person by twitter username.
         /// </summary>
         /// <param name="twitterUsername">The twitter username.</param>
+        /// <param name="queue">The queue.</param>
+        /// <param name="webhook">The webhook.</param>
         /// <returns></returns>
-        Person LookupPersonByTwitter(string twitterUsername);
+        Person LookupPersonByTwitter(string twitterUsername, int? queue = null, string webhook = null);
 
         /// <summary>
         /// Lookup Person by facebook username.
         /// </summary>
         /// <param name="facebookUsername">The facebook username.</param>
+        /// <param name="queue">The queue.</param>
+        /// <param name="webhook">The webhook.</param>
         /// <returns></returns>
-        Person LookupPersonByFacebook(string facebookUsername);
+        Person LookupPersonByFacebook(string facebookUsername, int? queue = null, string webhook = null);
     }
 }
