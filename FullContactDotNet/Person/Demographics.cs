a51e49bdde8c3fc6bee24217c8c16f994a29a987
Adam Valverde <adamv@brushfireinteractive.com>
2015-08-25 11:39:23 -0700

    adding missing age and age range from demo info

--- FullContactDotNet/Person/Demographics.cs
+++ FullContactDotNet/Person/Demographics.cs
@@ -3,12 +3,20 @@
     public class Demographics
     {
         /// <summary>
-        /// Gets or sets the location deduced.
+        /// Gets or sets the age.
         /// </summary>
         /// <value>
-        /// The location deduced.
+        /// The age.
         /// </value>
-        public LocationDeduced LocationDeduced { get; set; }
+        public string Age { get; set; }
+
+        /// <summary>
+        /// Gets or sets the age range.
+        /// </summary>
+        /// <value>
+        /// The age range.
+        /// </value>
+        public string AgeRange { get; set; }
 
         /// <summary>
         /// Gets or sets the gender.
@@ -17,6 +25,14 @@
         /// The gender.
         /// </value>
         public string Gender { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location deduced.
+        /// </summary>
+        /// <value>
+        /// The location deduced.
+        /// </value>
+        public LocationDeduced LocationDeduced { get; set; }
 
         /// <summary>
         /// Gets or sets the location general.
